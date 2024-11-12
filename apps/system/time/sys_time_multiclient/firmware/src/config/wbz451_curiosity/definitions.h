59aad07b91ca9ed01c98c2dd6fb16324420926be
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-12 14:53:34 +0530

    [SYS_TIME] Regenerated demo for system__time__sys_time_multiclient__firmware__wbz451_curiosity.X
    MH3-102968

--- apps/system/time/sys_time_multiclient/firmware/src/config/wbz451_curiosity/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/wbz451_curiosity/definitions.h
@@ -57,11 +57,11 @@
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "bsp/bsp.h"
 #include "peripheral/tc/plib_tc0.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -75,13 +75,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "WBZ451"
-#define DEVICE_ARCH			 "CORTEX-M4"
-#define DEVICE_FAMILY		 "PIC32CX_BZ2"
-#define DEVICE_SERIES		 "BZ45"
+#define DEVICE_NAME          "WBZ451"
+#define DEVICE_ARCH          "CORTEX-M4"
+#define DEVICE_FAMILY        "PIC32CX_BZ2"
+#define DEVICE_SERIES        "BZ45"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 64000000
+#define CPU_CLOCK_FREQUENCY 64000000U
 
 // *****************************************************************************
 // *****************************************************************************
